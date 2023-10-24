4c653b9c70cbd90de1d14496d6755f607a723870
Jason Walker <jason.walker19937@gmail.com>
2023-10-24 12:47:33 +0100

    Updated GridPlacementSystem to show a mouse indicator, as well as updated scene.
    Imported cursor indicators to show where on the grid the mouse is pointing.

--- SuperNaturalTD/Assets/Scripts/GridBuildingSystem/GridPlacementSystem.cs
+++ SuperNaturalTD/Assets/Scripts/GridBuildingSystem/GridPlacementSystem.cs
@@ -8,12 +8,18 @@ public class GridPlacementSystem : MonoBehaviour
     MouseInputManager mouseInputManager;
 
     [SerializeField]
-    private GameObject mouseIndicator;
+    private GameObject mouseIndicator, cellIndicator;
+
+    [SerializeField]
+    private Grid grid;
 
     private void Update()
     {
         Vector3 mousePosition = mouseInputManager.GetSelectedMousePosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
+        gridPosition.y = 1;
+        cellIndicator.transform.position = gridPosition;
     }
 
 }
